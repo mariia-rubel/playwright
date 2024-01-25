3d83761e10ea292f32e726d2b5b534cbf5872b67
Mariia <mariiarubel@gmail.com>
2024-01-25 12:10:18 +0000

    removed the headed browsers

--- tests_storefront/conftest.py
+++ tests_storefront/conftest.py
@@ -15,10 +15,10 @@ def set_up (page):
     yield page
 
 @pytest.fixture()
-def set_up_pdf(playwright: Playwright):
-    browser = (playwright.firefox.launch(headless=False))
-    context = browser.new_context()
-    page = context.new_page()
+def set_up_pdf(page):
+    # browser = (playwright.firefox.launch(headless=False))
+    # context = browser.new_context()
+    # page = context.new_page()
     page.goto("https://online2pdf.com/")
     page.set_default_timeout(15000)
 
